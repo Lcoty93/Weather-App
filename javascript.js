4b5b2448fb4d4cdb21b8c85ad01a01f5c6174868
Luke Coty <Lukecoty4@gmail.com>
2022-06-22 10:46:59 -0400

    Release version 1.0.0

---
+++ javascript.js
@@ -0,0 +1,33 @@
+const btn = document.querySelector('#searchBox');
+const textBox = document.querySelector('#textBox');
+const divContainer = document.querySelector('#container');
+const weatherForecast = document.querySelector('#weatherForecast');
+const description = document.querySelector('#description');
+const locationAt = document.querySelector('#locationAt');
+const minMax = document.querySelector('#minMax');
+
+btn.addEventListener('click', () => {
+    const searchResult = textBox.value;
+    weatherUpdate(searchResult);
+})
+
+
+function weatherUpdate(searchResult){
+    fetch(`https://api.openweathermap.org/data/2.5/weather?q=${searchResult}&APPID=dcc04f824f06ac2b2f8a7d8bd6f54ea3&units=imperial`, {mode: 'cors'})
+    .then(function(response) {
+        return response.json();
+    })
+    .then(function(response) {
+        locationAt.textContent = `${searchResult}, ` + response.sys.country;
+
+        weatherForecast.textContent = Math.round(response.main.temp) + "°F";
+
+        description.textContent = 
+        "Feels like " + Math.round(response.main.feels_like) + "°F. " +
+        response.weather[0].description + ". " +
+        "Humidity: " + response.main.humidity + "%";
+        
+        minMax.textContent = "Low: " + Math.round(response.main.temp_min) + " High: " + Math.round(response.main.temp_max);
+
+    });
+}
